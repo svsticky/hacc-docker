68ee1f3d5f48d4ce6da744a8f137886b2b95bab9
Julius de Jeu <julius@voidcorp.nl>
2021-01-07 21:06:59 +0100

    Add rust and python example
    and fix cs and haskell

--- cs/Program.cs
+++ cs/Program.cs
@@ -6,7 +6,7 @@ namespace mnt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
